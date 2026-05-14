4e7eee4257e2eb08a5412836b1d257c31c093f64
Cédric Belin <cedx@outlook.com>
2026-05-14 22:43:51 +0200

    Code formatting

--- src/Release.psm1
+++ src/Release.psm1
@@ -42,7 +42,7 @@ class Release {
 		Initializes the class.
 	#>
 	static Release() {
-		[Release]::Data = (Import-PowerShellDataFile "$PSScriptRoot/ReleaseData.psd1").Releases.ForEach{ [Release] $_ }
+		[Release]::Data = foreach ($release in (Import-PowerShellDataFile "$PSScriptRoot/ReleaseData.psd1").Releases) { [Release] $release }
 	}
 
 	<#
