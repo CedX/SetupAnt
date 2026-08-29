70ac25cebac65ba91f991b686eced217ced74593
Cédric Belin <cedx@outlook.com>
2026-08-29 02:04:44 +0200

    Module merging

---
+++ Sources/Release.ps1
@@ -0,0 +1,132 @@
+﻿using namespace System.Diagnostics.CodeAnalysis
+using module ./Release.psm1
+using module ./Setup.psm1
+
+<#
+.SYNOPSIS
+	Finds a release that matches the specified version constraint.
+.INPUTS
+	The version constraint.
+.OUTPUTS
+	The release corresponding to the specified constraint, or `$null` if not found.
+#>
+function Find-Release {
+	[CmdletBinding()]
+	[OutputType([Release])]
+	param (
+		# The version constraint.
+		[Parameter(Mandatory, Position = 1, ValueFromPipeline)]
+		[string] $Constraint
+	)
+
+	process {
+		[Release]::Find($Constraint)
+	}
+}
+
+<#
+.SYNOPSIS
+	Gets the release corresponding to the specified version.
+.INPUTS
+	A string that contains a version number.
+.OUTPUTS
+	The release corresponding to the specified version, or `$null` if not found.
+#>
+function Get-Release {
+	[CmdletBinding()]
+	[OutputType([Release])]
+	param (
+		# The version number. Use `*` or `Latest` to get the latest release.
+		[Parameter(Mandatory, Position = 1, ValueFromPipeline)]
+		[string] $Version
+	)
+
+	process {
+		$Version -in "*", "Latest" ? [Release]::Latest() : [Release]::Get($Version)
+	}
+}
+
+<#
+.SYNOPSIS
+	Installs Apache Ant, after downloading it.
+.INPUTS
+	[string] The version constraint of the release to be installed.
+.INPUTS
+	[Release] The release to be installed.
+.OUTPUTS
+	The path to the installation directory.
+#>
+function Install-Release {
+	[CmdletBinding(DefaultParameterSetName = "Constraint")]
+	[OutputType([string])]
+	param (
+		# The version constraint of the release to be installed.
+		[Parameter(Mandatory, ParameterSetName = "Constraint", Position = 1, ValueFromPipeline)]
+		[string] $Constraint,
+
+		# The instance of the release to be installed.
+		[Parameter(Mandatory, ParameterSetName = "InputObject", ValueFromPipeline)]
+		[Release] $InputObject,
+
+		# Value indicating whether to fetch the Ant optional tasks.
+		[switch] $OptionalTasks
+	)
+
+	process {
+		$release = $InputObject ? $InputObject : [Release]::Find($Constraint)
+		if (${release}?.Exists()) { [Setup]::new($release).Install($OptionalTasks) }
+		else { Write-Error "No release matches the specified version constraint." -Category ObjectNotFound }
+	}
+}
+
+<#
+.SYNOPSIS
+	Creates a new release.
+.INPUTS
+	The version number.
+.OUTPUTS
+	The newly created release.
+#>
+function New-Release {
+	[CmdletBinding()]
+	[OutputType([Release])]
+	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions", "")]
+	param (
+		# The version number.
+		[Parameter(Mandatory, Position = 1, ValueFromPipeline)]
+		[semver] $Version
+	)
+
+	process {
+		[Release]::new($Version)
+	}
+}
+
+<#
+.SYNOPSIS
+	Gets a value indicating whether a release with the specified version exists.
+.INPUTS
+	[string] The version number of the release to be tested.
+.INPUTS
+	[Release] The release to be tested.
+.OUTPUTS
+	`$true` if a release with the specified version exists, otherwise `$false`.
+#>
+function Test-Release {
+	[CmdletBinding(DefaultParameterSetName = "Version")]
+	[OutputType([bool])]
+	param (
+		# The version number of the release to be tested.
+		[Parameter(Mandatory, ParameterSetName = "Version", Position = 1, ValueFromPipeline)]
+		[semver] $Version,
+
+		# The release to be tested.
+		[Parameter(Mandatory, ParameterSetName = "InputObject", ValueFromPipeline)]
+		[Release] $InputObject
+	)
+
+	process {
+		$release = $InputObject ? $InputObject : [Release]::new($Version)
+		$release.Exists()
+	}
+}
--- Tests/Release.Tests.ps1
+++ Tests/Release.Tests.ps1
@@ -66,3 +66,69 @@ Describe "Release" {
 		}
 	}
 }
+
+<#
+.SYNOPSIS
+	Tests the features of the `Find-Release` cmdlet.
+#>
+Describe "Find-Release" {
+	BeforeAll { . "$PSScriptRoot/BeforeAll.ps1" }
+
+	It "should return `$null if no release matches the version constraint" {
+		Should-BeNull (Find-AntRelease $nonExistingRelease.Version)
+	}
+
+	It "should return the release corresponding to the version constraint if it exists" {
+		Should-BeSame $latestRelease (Find-AntRelease "latest")
+		Should-BeSame $latestRelease (Find-AntRelease "*")
+		Should-BeSame $latestRelease (Find-AntRelease "1")
+		Should-BeNull (Find-AntRelease "2")
+		Should-BeNull (Find-AntRelease ">1.10.17")?.Version
+		Should-Be "1.8.2" (Find-AntRelease "=1.8.2")?.Version
+		Should-Be "1.9.16" (Find-AntRelease "<1.10")?.Version
+		Should-Be "1.10.0" (Find-AntRelease "<=1.10")?.Version
+	}
+
+	It "should throw if the version constraint is invalid" -ForEach "abc", "?1.10" {
+		Should-Throw -ScriptBlock { Find-AntRelease $_ -ErrorAction Stop }
+	}
+}
+
+<#
+.SYNOPSIS
+	Tests the features of the `Get-Release` cmdlet.
+#>
+Describe "Get-Release" {
+	BeforeAll { . "$PSScriptRoot/BeforeAll.ps1" }
+
+	It "should return `$null if no release matches to the version number" {
+		Should-BeNull (Get-AntRelease $nonExistingRelease.Version)
+	}
+
+	It "should return the release corresponding to the version number if it exists" {
+		Should-Be "1.8.2" (Get-AntRelease "1.8.2")?.Version
+	}
+}
+
+<#
+.SYNOPSIS
+	Tests the features of the `Test-Release` cmdlet.
+#>
+Describe "Test-Release" {
+	BeforeAll { . "$PSScriptRoot/BeforeAll.ps1" }
+
+	It "should return `$true for the latest release" {
+		Should-BeTrue (Test-AntRelease $latestRelease.Version)
+		Should-BeTrue ($latestRelease | Test-AntRelease)
+	}
+
+	It "should return `$true if the release exists" {
+		Should-BeTrue (Test-AntRelease $existingRelease.Version)
+		Should-BeTrue ($existingRelease | Test-AntRelease)
+	}
+
+	It "should return `$false if the release does not exist" {
+		Should-BeFalse (Test-AntRelease $nonExistingRelease.Version)
+		Should-BeFalse ($nonExistingRelease | Test-AntRelease)
+	}
+}
